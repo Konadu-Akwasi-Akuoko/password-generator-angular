de67d7ac926e22b670d557ee7db9b58005149a51
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-04-08 16:55:18 +0000

    test: add tests to header component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('password-generator');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Password Generator App');
-  });
+
 });
--- src/app/components/password-generator/password-generator.component.spec.ts
+++ src/app/components/password-generator/password-generator.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PasswordGeneratorComponent } from './password-generator.component';
+import { By } from '@angular/platform-browser';
 
 describe('PasswordGeneratorComponent', () => {
   let component: PasswordGeneratorComponent;
@@ -8,10 +9,9 @@ describe('PasswordGeneratorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PasswordGeneratorComponent]
-    })
-    .compileComponents();
-    
+      imports: [PasswordGeneratorComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PasswordGeneratorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,25 @@ describe('PasswordGeneratorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have an h1', () => {
+    const h1 = fixture.debugElement.query(By.css('h1'));
+    expect(h1).toBeTruthy();
+    expect(h1.nativeElement.textContent).toBe('Password Generator');
+  });
+
+  it('should have a password input field', () => {
+    const passwordInput = fixture.debugElement.query(
+      By.css('input[name="password"]')
+    );
+    expect(passwordInput).toBeTruthy();
+  });
+
+  it('should have a copy password button', () => {
+    const copyButton = fixture.debugElement.query(
+      By.css('button[title="Copy password"]')
+    );
+    expect(copyButton).toBeTruthy();
   });
 });
