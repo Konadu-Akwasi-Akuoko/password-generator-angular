6cc180cea3e5d5de233c506cdc8ce9505c722a0c
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-04-09 14:21:00 +0000

    feat: added functionality to the slider, so that it can update the required observable

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PasswordGeneratorService } from '../../services/password-generator.service';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
   imports: [],
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.css'
+  styleUrl: './slider.component.css',
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit, OnDestroy {
+  sliderValue: number;
 
+  constructor(private passwordGeneratorService: PasswordGeneratorService) {
+    this.sliderValue = 0;
+  }
+
+  ngOnInit(): void {
+    this.passwordGeneratorService.passwordLength.subscribe((value) => {
+      this.sliderValue = value;
+    });
+    this.passwordGeneratorService.setPasswordLength(10);
+  }
+
+  ngOnDestroy(): void {
+    this.passwordGeneratorService.passwordLength.unsubscribe();
+  }
+
+  updateSliderValue(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.passwordGeneratorService.setPasswordLength(Number(target.value));
+  }
 }
