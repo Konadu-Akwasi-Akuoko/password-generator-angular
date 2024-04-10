f9e666afc30a07a711c6487ae229612c4696a512
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-04-10 12:17:58 +0000

    feat: implemented copy functionality

--- src/app/components/password-generator/password-generator.component.ts
+++ src/app/components/password-generator/password-generator.component.ts
@@ -1,18 +1,25 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { CheckboxComponent } from '../checkbox/checkbox.component';
-import { StrengthCheckerComponent } from '../strength-checker/strength-checker.component';
-import { SliderComponent } from '../slider/slider.component';
 import { PasswordGeneratorService } from '../../services/password-generator.service';
+import { CheckboxComponent } from '../checkbox/checkbox.component';
+import { SliderComponent } from '../slider/slider.component';
+import { StrengthCheckerComponent } from '../strength-checker/strength-checker.component';
 
 @Component({
   selector: 'app-password-generator',
   standalone: true,
-  imports: [CheckboxComponent, StrengthCheckerComponent, SliderComponent],
+  imports: [
+    CheckboxComponent,
+    StrengthCheckerComponent,
+    SliderComponent,
+    UpperCasePipe,
+  ],
   templateUrl: './password-generator.component.html',
   styleUrl: './password-generator.component.css',
 })
 export class PasswordGeneratorComponent implements OnInit, OnDestroy {
   generatedPassword: string;
+  onPasswordCopied: boolean = false;
 
   constructor(private passwordGeneratorService: PasswordGeneratorService) {
     this.generatedPassword = '';
@@ -30,5 +37,13 @@ export class PasswordGeneratorComponent implements OnInit, OnDestroy {
 
   onGeneratePasswordClick() {
     this.passwordGeneratorService.generatePassword();
+  }
+
+  onCopyButtonClicked() {
+    navigator.clipboard.writeText(this.generatedPassword);
+    this.onPasswordCopied = true;
+    setTimeout(() => {
+      this.onPasswordCopied = false;
+    }, 2000);
   }
 }
